c020482b55ef00b89b88be247aef4d221d63ca96
davidcooper7 <dcooper6@hawk.iit.edu>
2024-12-12 15:29:18 -0600

    Add argument parsing

--- RUN_FULTONMARKET.py
+++ RUN_FULTONMARKET.py
@@ -12,40 +12,43 @@ PARAMETERS:
 """
 
 
-import os, sys
+import os, sys, argparse
 sys.path.append('FultonMarket')
 from FultonMarket import FultonMarket
 
-# Inputs
-input_dir = sys.argv[1]
-name = sys.argv[2]
-input_sys = os.path.join(input_dir, name+'_sys.xml')
-input_state = os.path.join(input_dir, name+'_state.xml')
-input_pdb = os.path.join(input_dir, name+'.pdb')
-print(input_pdb)
 
-# Outputs
-rep = int(sys.argv[4])
-output_dir = os.path.join(sys.argv[3], name + '_' + str(rep))
-if not os.path.exists(output_dir):
-    os.mkdir(output_dir)
-assert os.path.exists(output_dir)
 
-# Simulation parameters
-try:
-    total_sim_time = int(sys.argv[5])
-except:
-    total_sim_time = 500
+if __name__ == '__main__':
 
-sub_sim_length = 50
+    # Arguments
+    parser == argparse.ArgumentParser()
+    parser.add_argument('input_dir', help="absolute path to the directory with input xml and pdb")
+    parser.add_argument('name', help="pdb file before the extension")
+    parser.add_argument('output_dir', help="absolute path to the directory where a subdirectory with output will be stored")
+    parser.add_argument('replicate', help="replicate number of simulation. THIS MUST BE SPECIFIED to avoid accidental overwritting")
+    parser.add_argument('-t', '--total-sim-time', default=500, help="total simulation aggregate time. Default is 500 ns.", type=int)
+    parser.add_argument('-s', '--sub-sim-length', default=50, help="time of sub simulations. Default is 50 ns. Recommended this is smaller for larger systems.", type=int)
+    parser.add_argument('-n', '--n-replica', default=100, help="number of replica to start with between T_min (300 K) and T_max (360 K)", type=int)
+    args = parser.parse_args()
 
-try:
-    n_replicates = int(sys.argv[6])
-except:
-    n_replicates = 90
 
-# Run rep exchange
-market = FultonMarket(input_pdb=input_pdb, input_system=input_sys, input_state=input_state, n_replicates=n_replicates)
+    
+    # Inputs
+    input_dir = args.input_dir
+    name = args.name
+    input_sys = os.path.join(input_dir, name+'_sys.xml')
+    input_state = os.path.join(input_dir, name+'_state.xml')
+    input_pdb = os.path.join(input_dir, name+'.pdb')
+    
+    # Outputs
+    output_dir = os.path.join(sys.argv[3], name + '_' + str(args.replicate))
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+    assert os.path.exists(output_dir)
 
-market.run(total_sim_time=total_sim_time, iter_length=0.001, sim_length=sub_sim_length, output_dir=output_dir)
+    
+    # Run rep exchange
+    market = FultonMarket(input_pdb=input_pdb, input_system=input_sys, input_state=input_state, n_replicates=args.n)
+    
+    market.run(total_sim_time=args.t, iter_length=0.001, sim_length=args.s, output_dir=output_dir)
 
