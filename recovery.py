f43497ba3ba2c803387324b2b85609955e53d411
davidcooper7 <dcooper6@hawk.iit.edu>
2025-01-21 11:20:18 -0600

    Add recovery.py

---
+++ recovery.py
@@ -0,0 +1,33 @@
+"""
+Run this script with OUT_OF_MEMORY error occurs and then continue using RUN_FULTONMARKET.py
+"""
+import os, sys
+import numpy as np
+import netCDF4 as nc
+from openmmtools.multistate import MultiStateReporter
+from FultonMarket.FultonMarketUtils import truncate_ncdf
+
+
+# Set output directory
+output_dir = sys.argv[1]
+save_dir = os.path.join(output_dir, 'saved_variables')
+sub_sim_save_dir = os.path.join(save_dir, str(len(os.listdir(save_dir))))
+
+# Create reporter
+ncdf_fn = os.path.join(output_dir, 'output.ncdf')
+reporter = MultiStateReporter(ncdf_fn)
+reporter.open()
+
+# Truncate
+pos, velos, box_vecs, states, energies, temps = truncate_ncdf(ncdf_fn, ncdf_fn, reporter)
+
+# Save
+np.save(os.path.join(sub_sim_save_dir, 'positions.npy'), pos.data)
+np.save(os.path.join(sub_sim_save_dir, 'velocities.npy'), velos.data)
+np.save(os.path.join(sub_sim_save_dir, 'box_vectors.npy'), box_vecs.data)
+np.save(os.path.join(sub_sim_save_dir, 'states.npy'), states.data)
+np.save(os.path.join(sub_sim_save_dir, 'energies.npy'), energies.data)
+np.save(os.path.join(sub_sim_save_dir, 'temperatures.npy'), temps.data)
+
+
+
